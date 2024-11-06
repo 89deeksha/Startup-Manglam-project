d3345da28393ca305e88a77bb411547dcb7000cf
89deeksha <deekshajain0310@gmail.com>
2024-11-06 16:55:53 +0530

    file added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div id="full-banner">
+      <div id="image">
+        <div id="img"></div>
+        <div id="graph"></div>
+        <div id="Flip-arrow"></div>
+        <div id="blue-circle">
+          <div id="inner-circle"></div>
+        </div>
+        <div id="green-card"></div>
+        {/* <div id="game-icons:lob-arrow"> */}
+
+        {/* </div> */}
+        <div id="white-box"></div>
+        <div id="orange-circle"></div>
+        <div id="pink-circle"></div>
+      </div>
+      <div>
+        <p id="content">Do Yoga Today For Better A Tomorrow</p>
+        <div id="emoji"></div>
+        <button id="BTN">Join Now</button>
+        <div id="below-BTn">
+          <p id="Below-BTN">1000+ </p>
+          <p id="happy-cust">Happy Customers</p>
+        </div>
+
+        <div id="pnk-circle"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div id="page">
+      <div id="navbar">
+        <div id="nav-1">
+          <p>About us</p>
+          <p>Classes</p>
+          <p>Trainers</p>
+        </div>
+        <button id="nav-2">BECOME A MEMBER</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
